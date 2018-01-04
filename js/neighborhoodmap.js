9cc71339860b67b754a3292c4c348af1835739d4
Brian Johnson <jb6884@yahoo.com>
2018-01-04 15:57:11 -0600

    EOD commit.. working on buildInfoWindow function

--- js/neighborhoodmap.js
+++ js/neighborhoodmap.js
@@ -91,10 +91,16 @@ var ViewModel = function() {
 	// Create a new blank array for all the listing markers.
 	var markers = [];
 	var currentMark;
-	
+
+	function buildInfoWindow(infowindow,map,marker) {
+		console.log('hello');
+		infowindow.open(map,marker);
+		infowindow.setContent('<div>' + marker.title + ' - ' + marker.id + '</div><div id="pano"></div>');
+
+	}
 	// Function to build the map markers and infowWindows
 	function buildMarkers(map) {
-
+		var currentMark, countItems = 0;
 		ko.utils.arrayForEach(self.placeList(), function(item) {
 			var markerCoords = {lat: item.lat(), lng: item.lng()};
 			
@@ -109,7 +115,8 @@ var ViewModel = function() {
 				map: map,
 				animation: google.maps.Animation.DROP,
 				title: item.name(),
-				icon: defaultIcon
+				icon: defaultIcon,
+				id: countItems
 			});
 			//Push onto an array to track our markers so we can access them later easily
 			markers.push(marker);
@@ -126,18 +133,18 @@ var ViewModel = function() {
 			marker.addListener('click', function() {
 				// var to keep a pointer to the 'this' scope
 				var that = this;
-				infowindow.open(map,marker);
+				
+				buildInfoWindow(infowindow,map,marker);
+
+				//infowindow.open(map,marker);   
 				// Animate the marker for 3 seconds
 				this.setAnimation(google.maps.Animation.BOUNCE);
 				setTimeout(function(){ that.setAnimation(null) }, 3000);
 				currentMark = that;
 				that.setIcon(selectedIcon);
 			});
-
-
-
+			countItems++;
 		});
-
 		
 	};
 
@@ -188,6 +195,55 @@ var ViewModel = function() {
 		new google.maps.Size(21,34));
 		return markerImage;
 	};
+
+	// Code Source:  Google API Course
+	// This is the PLACE DETAILS search - it's the most detailed so it's only
+	// executed when a marker is selected, indicating the user wants more
+	// details about that place.
+	function getPlacesDetails(marker, infowindow) {
+	  var service = new google.maps.places.PlacesService(map);
+	  service.getDetails({
+		placeId: marker.id
+	  }, function(place, status) {
+		if (status === google.maps.places.PlacesServiceStatus.OK) {
+		  // Set the marker property on this infowindow so it isn't created again.
+		  infowindow.marker = marker;
+		  var innerHTML = '<div>';
+		  if (place.name) {
+			innerHTML += '<strong>' + place.name + '</strong>';
+		  }
+		  if (place.formatted_address) {
+			innerHTML += '<br>' + place.formatted_address;
+		  }
+		  if (place.formatted_phone_number) {
+			innerHTML += '<br>' + place.formatted_phone_number;
+		  }
+		  if (place.opening_hours) {
+			innerHTML += '<br><br><strong>Hours:</strong><br>' +
+				place.opening_hours.weekday_text[0] + '<br>' +
+				place.opening_hours.weekday_text[1] + '<br>' +
+				place.opening_hours.weekday_text[2] + '<br>' +
+				place.opening_hours.weekday_text[3] + '<br>' +
+				place.opening_hours.weekday_text[4] + '<br>' +
+				place.opening_hours.weekday_text[5] + '<br>' +
+				place.opening_hours.weekday_text[6];
+		  }
+		  if (place.photos) {
+			innerHTML += '<br><br><img src="' + place.photos[0].getUrl(
+				{maxHeight: 100, maxWidth: 200}) + '">';
+		  }
+		  innerHTML += '</div>';
+		  infowindow.setContent(innerHTML);
+		  infowindow.open(map, marker);
+		  // Make sure the marker property is cleared if the infowindow is closed.
+		  infowindow.addListener('closeclick', function() {
+			infowindow.marker = null;
+		  });
+		} else {
+			alert('Get Place Failed...')
+		}
+	  });
+	}
 
 
 
