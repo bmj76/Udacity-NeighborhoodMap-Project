a2f528a8a51d2071c686d189998fbb5e49127497
Brian Johnson <jb6884@yahoo.com>
2018-01-04 09:43:42 -0600

    Added Bootstrap modal window for the basic UI design.  Google Map custom control button

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # Udacity-NeighborhoodMap-Project
 
+This is a single page application that shows places that I have defined in Manchester, Missouri where I was raised.  I still visit this part of town frequently, especially for a few restaurants that my family enjoys.
 
+The application shows a Google map with markers of these places.  Clicking the markers will.. TBD
+
+## Developer Information
+
+Bower was used to manage dependencies.  Please review the bower.json file for more information.
+
+The bootstrap library is used to ensure responsive design.  A bootstrap modal window is used to create an overlay when the Filter Markers button is pushed.
+
+jQuery is required by bootstrap.
+
+Knockout JS is used for MV-VM code organization as well as declartive bindings and automatic UI refresh
+
+This project uses the Google Maps API to generate the Map, Markers, InfoWindows and custom control button
 
 ## References
 
-Bootstrap Navbar: [https://getbootstrap.com/docs/4.0/components/navbar/ ]
+Bootstrap Modal Window: [https://v4-alpha.getbootstrap.com/components/modal/#examples]
+Google Map Custom Control: [https://developers.google.com/maps/documentation/javascript/examples/control-custom]
--- js/neighborhoodmap.js
+++ js/neighborhoodmap.js
@@ -62,6 +62,40 @@ var ViewModel = function() {
 
 	}
 
+
+	function FilterControl(controlDiv, map) {
+
+        // Set CSS for the control border.
+        var controlUI = document.createElement('div');
+        controlUI.style.backgroundColor = '#fff';
+        controlUI.style.border = '2px solid #fff';
+        controlUI.style.borderRadius = '3px';
+        controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+        controlUI.style.cursor = 'pointer';
+        controlUI.style.marginBottom = '22px';
+        controlUI.style.textAlign = 'center';
+        controlUI.title = 'Click to Open the Map Filter';
+        controlDiv.appendChild(controlUI);
+
+        // Set CSS for the control interior.
+        var controlText = document.createElement('div');
+        controlText.style.color = 'rgb(25,25,25)';
+        controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+        controlText.style.fontSize = '16px';
+        controlText.style.lineHeight = '38px';
+        controlText.style.paddingLeft = '5px';
+        controlText.style.paddingRight = '5px';
+        controlText.innerHTML = 'Filter Markers';
+        controlUI.appendChild(controlText);
+
+        // Setup the click event listeners: Open (or close) the modal filter window
+        controlUI.addEventListener('click', function() {
+          $('#filterModal').modal('toggle');
+        });
+
+      }
+
+
 	var map;
 
 	/* Initialize map */
@@ -71,6 +105,14 @@ var ViewModel = function() {
           center: {lat: 38.594754, lng: -90.519612},
           zoom: 18
         });
+
+         // Create the DIV to hold the control and call the CenterControl()
+        // constructor passing in this DIV.
+        var filterControlDiv = document.createElement('div');
+        var filterControl = new FilterControl(filterControlDiv, map);
+
+        filterControlDiv.index = 1;
+        map.controls[google.maps.ControlPosition.TOP_CENTER].push(filterControlDiv);
         var markerCoords = {lat: 38.594754, lng: -90.519612};
         var marker = new google.maps.Marker({
           position: markerCoords,
