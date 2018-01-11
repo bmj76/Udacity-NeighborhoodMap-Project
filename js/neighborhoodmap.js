9df30426f09600e065c476825748847c978a98af
Brian Johnson <jb6884@yahoo.com>
2018-01-11 08:11:51 -0600

    Separated json into a different file and removed jquery for input box value update

--- js/neighborhoodmap.js
+++ js/neighborhoodmap.js
@@ -1,42 +1,4 @@
-// Set up an array of places that I visit.  This could be any number of items.
-var initialPlaces = [
-	{
-		name: 'Manee Thai',
-		type: 'Thai Cuisine',
-		url: 'http://maneethaistl.com',
-		lat: 38.594754,
-		lng: -90.519612
-	},
-	{
-		name: 'Joey B\'s Food and Drink',
-		type: 'American Cuisine & Seafood',
-		url: 'http://joeybsmanchester.com',
-		lat: 38.593922, 
-		lng: -90.519430
-	},
-	{
-		name: 'Tucker\'s Place',
-		type: 'Steakhouse',
-		url: 'www.tuckersplacestl.com/',
-		lat: 38.5919648, 
-		lng: -90.5072234
-	},
-	{
-		name: 'Shahrazad Mediterranean Restaurant',
-		type: 'Middle Eastern Cuisine',
-		url: 'http://shahrazadmanchester.com',
-		lat: 38.595669, 
-		lng: -90.522091
-	},
-	{
-		name: 'Culvers',
-		type: 'American Fast Food',
-		url: 'http://culvers.com',
-		lat: 38.592877,  
-		lng: -90.519349
-	}
 
-];
 
 //Build the knockout object
 var Place = function(data) {
@@ -291,9 +253,12 @@ var ViewModel = function() {
 			Window */
 		ko.utils.arrayForEach(self.placeList(), function(item) {
 			if (item != place) {
+				//Hides the marker that does not matched the clicked item
 				item.marker.setVisible(false);
 			} else {
-				$('#inputFilter').val(item.name());
+				/* 	Displays the marker that matches the clicked item
+					Updates the currentFilter value which updates the input box via knockout
+					Opens the InfoWindow */
 				item.marker.setVisible(true);
 				self.currentFilter(item.name());
 				item.marker.openMe(item.marker);
