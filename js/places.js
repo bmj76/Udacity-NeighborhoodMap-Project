63d235ca278e473619d0ac16d2a8a245961cf81a
Brian Johnson <jb6884@yahoo.com>
2018-01-11 08:13:29 -0600

    Adding places.js

---
+++ js/places.js
@@ -0,0 +1,39 @@
+// Set up an array of places that I visit.  This could be any number of items.
+var initialPlaces = [
+	{
+		name: 'Manee Thai',
+		type: 'Thai Cuisine',
+		url: 'http://maneethaistl.com',
+		lat: 38.594754,
+		lng: -90.519612
+	},
+	{
+		name: 'Joey B\'s Food and Drink',
+		type: 'American Cuisine & Seafood',
+		url: 'http://joeybsmanchester.com',
+		lat: 38.593922, 
+		lng: -90.519430
+	},
+	{
+		name: 'Tucker\'s Place',
+		type: 'Steakhouse',
+		url: 'www.tuckersplacestl.com/',
+		lat: 38.5919648, 
+		lng: -90.5072234
+	},
+	{
+		name: 'Shahrazad Mediterranean Restaurant',
+		type: 'Middle Eastern Cuisine',
+		url: 'http://shahrazadmanchester.com',
+		lat: 38.595669, 
+		lng: -90.522091
+	},
+	{
+		name: 'Culvers',
+		type: 'American Fast Food',
+		url: 'http://culvers.com',
+		lat: 38.592877,  
+		lng: -90.519349
+	}
+
+];
